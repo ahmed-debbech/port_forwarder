fa5d5ab5057e6d3a4fef4a2ed28fcadf5ca8c06a
ahmed-debbech <debbech.ahmed@gmail.com>
2025-03-06 01:38:34 +0100

    add dynamic refresh rate for bot

--- bot/bot.go
+++ bot/bot.go
@@ -6,13 +6,19 @@ import (
 	"time"
 	"net"
 	"fmt"
+	"strconv"
 )
 
 func main(){
 	log.Println("Bot started!")
 
-	if len(os.Args) < 3 {
-		log.Println("Usage: go run bot.go yourserverip secretPin")
+	if len(os.Args) < 4 {
+		log.Println("Usage: go run bot.go yourserverip secretPin refreshRateInSec")
+		return
+	}
+
+	refreshRate, err := strconv.Atoi(os.Args[3])
+	if err != nil {
 		return
 	}
 
@@ -20,7 +26,7 @@ func main(){
 		conn, err := net.Dial("tcp", fmt.Sprintf("%s:3150", os.Args[1]))
 		if err != nil {
 			log.Println("could not connect to server because:", err)
-			time.Sleep(time.Second * 10)
+			time.Sleep(time.Second * time.Duration(refreshRate))
 			continue
 		}
 	
@@ -28,12 +34,12 @@ func main(){
 		_, err = conn.Write([]byte(s))
 		if err != nil {
 			log.Println("could not send data to server because:", err)
-			time.Sleep(time.Second * 10)
+			time.Sleep(time.Second * time.Duration(refreshRate))
 			continue
 		}
 
 		log.Println("sent to server!")
 		conn.Close()
-		time.Sleep(time.Second * 10)
+		time.Sleep(time.Second * time.Duration(refreshRate))
 	}
 }
